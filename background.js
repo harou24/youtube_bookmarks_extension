74e7cccda26d3c1b96d516ff8efae82b6239d701
Haroutioun Achtchian <haroutioun24@gmail.com>
2022-06-18 18:18:59 +0200

    getting started

---
+++ background.js
@@ -0,0 +1,12 @@
+
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    if (tab.url && tab.url.includes("youtube.com/watch")) {
+        const queryParams = tab.url.split("?")[1]
+        const urlParams = new URLSearchParams(queryParams)
+        console.log("URL_PARAMS->", urlParams)
+        chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            videoId: urlParams.get("v")
+        })
+    }
+})
