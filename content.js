79948a28916faaf5e5f8cd520916c389652dff19
Haroutioun Achtchian <haroutioun24@gmail.com>
2022-06-19 12:52:06 +0200

    progressing

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
+
 (() => {
-    let youtubeLeftControls, youtubePlayer
-    let currentVideo = ""
+    console.log("running content.js");
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+    let currentVideoBookmarks= [];
 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const {type, value, videoId} = obj
@@ -9,4 +12,41 @@
             newVideoLoaded()
         }
     })
+
+    const newVideoLoaded = async () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0]
+        console.log("BTN->", bookmarkBtnExists)
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement("img")
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png")
+            bookmarkBtn.className = "yt-btn " + "bookmark-btn"
+            bookmarkBtn.title = "Click to bookmark current timestamp!"
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0]
+            youtubePlayer = document.getElementsByClassName("video-stream")[0]
+
+            youtubeLeftControls.appendChild(bookmarkBtn)
+
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        }
+    }
+
+    const addNewBookmarkEventHandler = () => {
+        const currentTime = youtubePlayer.currentTime
+        const newBookmark = {
+            time: currentTime,
+            desc: "Bookmark at " + getTime(currentTime)
+        }
+        console.log("BOOKMARK->", newBookmark)
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a,b) => a.time - b.time))
+        })
+    }
+    newVideoLoaded()
 })()
+
+const getTime = t => {
+    var date = new Date(0)
+    date.setSeconds(t)
+    return date.toISOString().substr(11, 8)
+}
